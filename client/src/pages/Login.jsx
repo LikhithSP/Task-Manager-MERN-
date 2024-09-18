194c4abb447e6e030a67549f07020e7b6f3046ee
Likhith SP <splikith04@gmail.com>
2024-09-18 20:15:22 +0530

    Testing the setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+          <h1 className="text-3xl font-bold underline">
+      Hello World!
+    </h1>
+    </div>
   )
 }
 
